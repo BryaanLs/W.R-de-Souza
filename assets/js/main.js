6eda369d9569ffc65f1e9caa62a20fb1f60d20b3
bryaanls <bryanluccas@hotmail.com>
2023-03-24 15:15:44 -0300

    deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ const target = document.querySelectorAll('[data-anime]');
 const workCard = document.querySelectorAll('.work-card')
 
 function animeScroll() {
-    const windowTop = window.innerHeight * 0.7;
+    const windowTop = window.innerHeight * 0.5;
 
     target.forEach((e) => {
         if (e.getBoundingClientRect().top - windowTop < 120) {
